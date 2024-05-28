da6b9f5022af3daf9cb0cc233b6444957ca0de54
Dng Pham <phamdong9854@gmail.com>
2024-05-28 20:46:35 +0700

    update orderCtrl

--- src/apps/controllers/apis/order.js
+++ src/apps/controllers/apis/order.js
@@ -1,5 +1,11 @@
-const order = (req, res) => {
-    res.send("order");
+const getdata = require("../../../common/database");
+
+const order = async (req, res) => {
+    orders = await getdata(`SELECT order_id, SUM(price) FROM public.orderlines
+    left join products on products.product_id = orderlines.product_id
+    group by order_id
+    order by order_id`);
+    res.send(orders);
 }
 
 module.exports = {
